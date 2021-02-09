089b0e6e4b3df599d1c9d03449577fd0297d5ff9
Invictus <49228523+DevSerpent@users.noreply.github.com>
2021-02-09 15:22:51 +0100

    Added Prediction

--- V21/GameClient.h
+++ V21/GameClient.h
@@ -19,15 +19,16 @@ namespace HACKUZAN
 	public:
 		static GameClient* Instance;
 
+		/*
 		union
 		{
 			DEFINE_MEMBER_N(GameState GameState, Offsets::GameClient::GameState)
 		};
-
+		*/
 		static void PrintChat(const char* Message, DWORD color);
 		template<typename ...Args>
 		static void PrintChat(const char* message, DWORD color, Args ... args);
 
 		static bool StringEquals(const char* strA, const char* strB, bool ignore_case);
 	};
-}
+};

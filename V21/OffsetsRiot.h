c7c0cf2013f2a51ebc704567ea49304ca8c1a608
mlesterdampios <mlesterdampios@gmail.com>
2021-02-16 00:12:28 +0800

    Update OffsetsRiot.h
    FUCKING HELL
    THIS SHOULD FIX THE BANS NOW

--- V21/OffsetsRiot.h
+++ V21/OffsetsRiot.h
@@ -101,7 +101,7 @@ namespace V21 {
 			IssueOrderTrueReturn = 0x15C864,
 			
 			CastSpell = 0x4D5960,
-			TrueCastSpellReturn = 0x58A994,
+			TrueCastSpellReturn = 0x4EBAAC,
 
 			OnProcessSpellW = 0x4d7970,								// 11.3  
 			OnFinishCast = 0x4CD9C0,								// 11.3
